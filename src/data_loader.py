9974540650c721babb99c5011c00150833064baa
dianagamedi <da.gamez.diaz@gmail.com>
2019-11-12 22:18:09 +0100

    [DATALOADER] Add methods to get train and validation set

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,9 +3,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 
 
+def get_length_set(set):
+    return len(os.listdir(set))
+
+
 class DataLoader:
     """
-    Data loader to upload data set
+    Data loader to upload data set and manipulate data
 
     :param path_to_zip: path where the zip file containing the data set is located
 
@@ -13,8 +17,16 @@ class DataLoader:
 
     def __init__(self, path_to_zip):
         self.path_to_zip = path_to_zip
+        self.path = os.path.join(os.path.dirname(self.path_to_zip), 'cats_and_dogs_filtered')
+        self.train_dir = os.path.join(self.path, 'train')
+        self.validation_dir = os.path.join(self.path, 'validation')
 
     def get_path(self):
-        path = os.path.join(os.path.dirname(self.path_to_zip), 'cats_and_dogs_filtered')
+        return self.path
 
-        return path
+    def get_train_path_category(self, category):
+        return os.path.join(self.train_dir, category)
+
+    def get_validation_path_category(self, category):
+        return os.path.join(self.validation_dir, category)
+
